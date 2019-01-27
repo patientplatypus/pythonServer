101ac88c786408cdc158e70a16b9c77311df7581
Peter Weyand <pweyand@gmail.com>
2019-01-27 13:10:02 -0600

    added broken chunk example

--- python/barebone-serverless-flask-api-zeit-now/routes/routes.py
+++ python/barebone-serverless-flask-api-zeit-now/routes/routes.py
@@ -5,8 +5,16 @@ from flask import send_from_directory, request
 from index import app
 from werkzeug.utils import secure_filename
 import os
+import sqlite3
 
 ALLOWED_EXTENSIONS = set(['sqlite', 'jpg', 'txt'])
+
+# default to catch any route not already predefined
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return 'This path is not defined in routes: %s' % path
+
 
 @app.route('/getTEST', methods=['GET'])
 def test():
@@ -16,7 +24,7 @@ def allowed_file(filename):
     print("inside allowed_file")
     print("filename.rsplit('.', 1)[1].lower()")
     print(filename.rsplit('.', 1)[1].lower())
-    print(" ALLOWED_EXTENSIONS")
+    print("ALLOWED_EXTENSIONS")
     print(ALLOWED_EXTENSIONS)
     fileval = filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
     print("value of fileval")
@@ -24,7 +32,8 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['POST', 'GET'])
+# file upload does not work on lambda! no memory to save to!
+@app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         print("inside post method")
@@ -41,7 +50,7 @@ def upload_file():
             print("inside another if statement")
             return "No file selected"
         if file and allowed_file(file.filename):
-            print("inside last if statement")
+            print("allowed_file ok and file exists")
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return "file uploaded successfully!"
@@ -50,3 +59,50 @@ def upload_file():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
+
+@app.route('/firefox_places', methods=['POST'])
+def firefox_places():
+  print("inside firefox_places")
+  if 'file' not in request.files:
+    print("file not in request.files")
+    return "file not in request.files"
+  file = request.files['file']
+  if file.filename == '':
+    print("inside another if statement")
+    return "No file selected"
+  if file and allowed_file(file.filename):
+    print("allowed_file ok and file exists")
+    filename = secure_filename(file.filename)
+    conn = sqlite3.connect(filename)
+    print ("Operation done successfully")
+    conn.close()
+    print("after connect to sqlite3 db")
+    return "after sqlite query"
+
+
+@app.route("/upload_chunk_example", methods=["POST"])
+def upload_chunk_example():
+  print('inside /upload_chunk_example')
+  with open("/tmp/output_file", "bw") as f:
+    print("inside with open in upload_chunk")
+    chunk_size = 256
+    while True:
+      print("inside while true statement")
+      chunk = request.stream.read(chunk_size)
+      print("value of chunk")
+      print(chunk)
+      if len(chunk) == 0:
+        return
+      f.write(chunk)
+
+# from here: https://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
+
+# @app.route('/streaming_test', methods=['POST'])
+# def streaming_test():
+#     r = requests.get(url, stream=True)
+#     with open(local_filename, 'wb') as f:
+#         for chunk in r.iter_content(chunk_size=1024): 
+#             if chunk: # filter out keep-alive new chunks
+#                 f.write(chunk)
+#                 #f.flush() commented by recommendation from J.F.Sebastian
+#     return local_filename
